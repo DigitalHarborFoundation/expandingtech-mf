08d3b5a4293c6b2246999bd02d531fdfccaf852b
jonathanprozzi <jonathanprozzi@gmail.com>
2018-06-13 22:23:02 -0400

    added container and image styles for lesson

--- src/pages/test-lesson.js
+++ src/pages/test-lesson.js
@@ -4,15 +4,29 @@ import styled from 'styled-components'
 
 const TestLesson = ({ data }) => (
   <div>
-    <main>
+    <LessonContainer>
       <LessonTitle>{data.wordpressWpLessons.title}</LessonTitle>
       <LessonCopy
         dangerouslySetInnerHTML={{ __html: data.wordpressWpLessons.content }}
       />
       <Link to="/">Go back to the homepage</Link>
-    </main>
+    </LessonContainer>
   </div>
 )
+
+const LessonContainer = styled.main`
+  display: flex;
+  flex-direction: column;
+
+  img {
+    width: auto;
+    box-shadow: 0 3px 6px rgba(0, 0, 0, 0.16), 0 3px 6px rgba(0, 0, 0, 0.23);
+  }
+
+  img: hover {
+    box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px 10px rgba(0, 0, 0, 0.22);
+  }
+`
 
 const LessonTitle = styled.h1`
   font-family: avenir;
